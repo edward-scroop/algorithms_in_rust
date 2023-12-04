48191c486d3e953e0de423419f5f0e736f19bbbf
Edward Scroop <edward.scroop@gmail.com>
2023-12-04 17:50:04 +0800

    added stack implementation using linked list

--- src/stack.rs
+++ src/stack.rs
@@ -1,6 +1,13 @@
-pub struct Stack<T> {
-    stack: Vec<T>,
-    count: std::primitive::usize,
+trait Stack<T: Clone> {
+    fn push(&mut self, data: T);
+
+    fn pop(&mut self) -> Result<T, Error>;
+
+    fn peek(&self) -> Result<T, Error>;
+
+    fn count(&self) -> std::primitive::usize;
+
+    fn is_empty(&self) -> bool;
 }
 
 #[derive(Debug)]
@@ -22,30 +29,110 @@ impl std::fmt::Display for Error {
     }
 }
 
-impl<T> Stack<T> {
+pub struct VecStack<T> {
+    stack: Vec<T>,
+    count: std::primitive::usize,
+}
+
+impl<T> VecStack<T> {
     pub fn new() -> Self {
-        Stack {
+        VecStack {
             stack: Vec::<T>::new(),
             count: 0,
         }
     }
+}
 
-    pub fn count(&self) -> std::primitive::usize {
-        self.count
-    }
-
-    pub fn push(&mut self, data: T) {
+impl<T: Clone> Stack<T> for VecStack<T> {
+    fn push(&mut self, data: T) {
         self.stack.insert(self.count, data);
         self.count += 1;
     }
 
-    pub fn pop(&mut self) -> Result<T, Error> {
+    fn pop(&mut self) -> Result<T, Error> {
         if self.count == 0 {
             Err(Error::new("can not pop empty stack"))
         } else {
             self.count -= 1;
             Ok(self.stack.remove(self.count))
         }
+    }
+
+    fn peek(&self) -> Result<T, Error> {
+        if self.count == 0 {
+            Err(Error::new("stack is empty"))
+        } else {
+            Ok(self.stack[self.count - 1].clone())
+        }
+    }
+
+    fn count(&self) -> std::primitive::usize {
+        self.count
+    }
+
+    fn is_empty(&self) -> bool {
+        self.count == 0
+    }
+}
+
+pub struct ListStack<T> {
+    root_node: Option<Box<ListStackNode<T>>>,
+    count: std::primitive::usize,
+}
+
+struct ListStackNode<T> {
+    data: T,
+    next_node: Option<Box<ListStackNode<T>>>,
+}
+
+impl<T> ListStack<T> {
+    pub fn new() -> Self {
+        ListStack {
+            root_node: None,
+            count: 0,
+        }
+    }
+}
+
+impl<T> ListStackNode<T> {
+    fn new(data: T, next_node: Option<Box<ListStackNode<T>>>) -> Self {
+        ListStackNode { data, next_node }
+    }
+}
+
+impl<T: Clone> Stack<T> for ListStack<T> {
+    fn push(&mut self, data: T) {
+        let new_node = ListStackNode::new(data, self.root_node.take());
+        self.root_node = Some(Box::new(new_node));
+        self.count += 1;
+    }
+
+    fn pop(&mut self) -> Result<T, Error> {
+        if self.count == 0 {
+            Err(Error::new("can not pop empty stack"))
+        } else {
+            let mut old_root_node = self.root_node.take();
+            let new_node = old_root_node.as_mut().unwrap().next_node.take();
+            self.root_node = new_node;
+            self.count -= 1;
+            Ok(old_root_node.unwrap().data)
+        }
+    }
+
+    fn peek(&self) -> Result<T, Error> {
+        if self.count == 0 {
+            Err(Error::new("stack is empty"))
+        } else {
+            Ok(self.root_node.as_ref().unwrap().data.clone())
+        }
+    }
+
+    fn count(&self) -> std::primitive::usize {
+        self.count
+    }
+
+    fn is_empty(&self) -> bool {
+        self.count == 0
     }
 }
 
@@ -54,8 +141,34 @@ mod tests {
     use super::*;
 
     #[test]
-    fn stack() {
-        let mut test_stack = Stack::new();
+    fn vec_stack_count() {
+        let mut test_stack = VecStack::new();
+        let (test_data_1, test_data_2, test_data_3) = (10, 42, 59492957);
+
+        assert!(test_stack.count() == 0);
+        test_stack.push(test_data_1.clone());
+        assert!(test_stack.count() == 1);
+        test_stack.push(test_data_2.clone());
+        assert!(test_stack.count() == 2);
+        test_stack.push(test_data_3.clone());
+        assert!(test_stack.count() == 3);
+    }
+
+    #[test]
+    fn vec_stack_push() {
+        let mut test_stack = VecStack::new();
+        let (test_data_1, test_data_2, test_data_3) = (10, 42, 59492957);
+
+        test_stack.push(test_data_1.clone());
+        test_stack.push(test_data_2.clone());
+        test_stack.push(test_data_3.clone());
+        assert!(test_stack.count() == 3);
+        assert!(test_stack.is_empty() == false);
+    }
+
+    #[test]
+    fn vec_stack_pop() {
+        let mut test_stack = VecStack::new();
         let (test_data_1, test_data_2, test_data_3) = (10, 42, 59492957);
 
         test_stack.push(test_data_1.clone());
@@ -65,6 +178,91 @@ mod tests {
         assert!(test_stack.pop().unwrap() == test_data_3);
         assert!(test_stack.pop().unwrap() == test_data_2);
         assert!(test_stack.pop().unwrap() == test_data_1);
+
+        assert!(test_stack.count() == 0);
+        assert!(test_stack.is_empty());
         assert!(test_stack.pop().is_err());
+    }
+
+    #[test]
+    fn vec_stack_peek() {
+        let mut test_stack = VecStack::new();
+        let (test_data_1, test_data_2, test_data_3) = (10, 42, 59492957);
+
+        test_stack.push(test_data_1.clone());
+        test_stack.push(test_data_2.clone());
+        test_stack.push(test_data_3.clone());
+
+        assert!(test_stack.peek().unwrap() == test_data_3);
+        assert!(test_stack.pop().unwrap() == test_data_3);
+        assert!(test_stack.peek().unwrap() == test_data_2);
+        assert!(test_stack.pop().unwrap() == test_data_2);
+        assert!(test_stack.peek().unwrap() == test_data_1);
+        assert!(test_stack.pop().unwrap() == test_data_1);
+        assert!(test_stack.is_empty());
+        assert!(test_stack.peek().is_err());
+    }
+
+    #[test]
+    fn list_stack_count() {
+        let mut test_stack = ListStack::new();
+        let (test_data_1, test_data_2, test_data_3) = (10, 42, 59492957);
+
+        assert!(test_stack.count() == 0);
+        test_stack.push(test_data_1.clone());
+        assert!(test_stack.count() == 1);
+        test_stack.push(test_data_2.clone());
+        assert!(test_stack.count() == 2);
+        test_stack.push(test_data_3.clone());
+        assert!(test_stack.count() == 3);
+    }
+
+    #[test]
+    fn list_stack_push() {
+        let mut test_stack = ListStack::new();
+        let (test_data_1, test_data_2, test_data_3) = (10, 42, 59492957);
+
+        test_stack.push(test_data_1.clone());
+        test_stack.push(test_data_2.clone());
+        test_stack.push(test_data_3.clone());
+        assert!(test_stack.count() == 3);
+        assert!(test_stack.is_empty() == false);
+    }
+
+    #[test]
+    fn list_stack_pop() {
+        let mut test_stack = ListStack::new();
+        let (test_data_1, test_data_2, test_data_3) = (10, 42, 59492957);
+
+        test_stack.push(test_data_1.clone());
+        test_stack.push(test_data_2.clone());
+        test_stack.push(test_data_3.clone());
+
+        assert!(test_stack.pop().unwrap() == test_data_3);
+        assert!(test_stack.pop().unwrap() == test_data_2);
+        assert!(test_stack.pop().unwrap() == test_data_1);
+
+        assert!(test_stack.count() == 0);
+        assert!(test_stack.is_empty());
+        assert!(test_stack.pop().is_err());
+    }
+
+    #[test]
+    fn list_stack_peek() {
+        let mut test_stack = ListStack::new();
+        let (test_data_1, test_data_2, test_data_3) = (10, 42, 59492957);
+
+        test_stack.push(test_data_1.clone());
+        test_stack.push(test_data_2.clone());
+        test_stack.push(test_data_3.clone());
+
+        assert!(test_stack.peek().unwrap() == test_data_3);
+        assert!(test_stack.pop().unwrap() == test_data_3);
+        assert!(test_stack.peek().unwrap() == test_data_2);
+        assert!(test_stack.pop().unwrap() == test_data_2);
+        assert!(test_stack.peek().unwrap() == test_data_1);
+        assert!(test_stack.pop().unwrap() == test_data_1);
+        assert!(test_stack.is_empty());
+        assert!(test_stack.peek().is_err());
     }
 }
